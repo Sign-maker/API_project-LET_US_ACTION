8ac0eb57c2424c7bd62e00c3b8c0e8fb00535c42
TetianaL <4215654@gmail.com>
2024-04-13 11:41:55 +0300

    swagger docs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 import userRouter from "./routes/authRouter.js";
 
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/users", userRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
