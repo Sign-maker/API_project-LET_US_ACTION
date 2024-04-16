f98e350273a7dbdedd4301fdebd5a781c0867686
TetianaL <4215654@gmail.com>
2024-04-16 13:42:42 +0300

    added waterRouter

--- routes/waterRouter.js
+++ routes/waterRouter.js
@@ -1,13 +1,34 @@
 import express from "express";
 import validateBody from "../helpers/validateBody";
-import waterSchemas from "../schemas/waretSchemas";
+import waterSchemas from "../schemas/waterSchemas";
+import { authenticate } from "../middlewares/authenticate";
+import { isValidId } from "../middlewares/isValidId";
 
 const waterRouter = express.Router();
 
 waterRouter.post(
   "/water",
-  validateBody(waterSchemas),
+  authenticate,
+  validateBody(waterSchemas.waterSchema),
   waterController.addWater
 );
+
+waterRouter.patch(
+  "/water/:id",
+  authenticate,
+  validateBody(waterSchemas.updateWaterSchema),
+  waterController.updateWater
+);
+
+waterRouter.delete(
+  "/water/:id",
+  authenticate,
+  isValidId,
+  waterController.deleteWater
+);
+
+waterRouter.get("/today", authenticate, waterController.getByDay);
+
+waterRouter.get("/month/:month", authenticate, waterController.getByMonth);
 
 export default waterRouter;
