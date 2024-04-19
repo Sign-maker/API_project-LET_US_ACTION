1bbccc4cba4920349f444797b29588fe6b0f815f
Sign-maker <muratovdv76@gmail.com>
2024-04-20 01:21:39 +0300

    daily consyants fixed

--- constants/water-constants.js
+++ constants/water-constants.js
@@ -4,5 +4,5 @@ export const maxDailyWaterCount = 15000;
 export const waterLimits = {
   MIN_ONE_TIME_WATER_VALUE: 1,
   MAX_ONE_TIME_WATER_VALUE: 5000,
-  MAX_DAILY_WATER_VALUE: 5000,
+  MAX_DAILY_WATER_VALUE: 15000,
 };
